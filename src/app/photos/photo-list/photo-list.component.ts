b4d8742822bf9b0fe11c5a2bbc7f045c6b17e358
Doug <douglas.souza@sinqia.com.br>
2022-01-17 15:30:48 -0300

    Implementacao do padrao de projeto debounce para possibilitar o ganho de performance na aplicacao

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,7 +1,9 @@
 import { Photo } from './../photo/photo';
-import { Component, OnInit, Output } from '@angular/core';
+import { Component, OnDestroy, OnInit, Output } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { PhotoService } from '../photo/photo.service';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators'
+
 
 
 @Component({
@@ -9,18 +11,27 @@ import { PhotoService } from '../photo/photo.service';
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
 
 
   photos: Photo[] = [];
-  filter: string ="";
+  filter: string ='';
+  debounce: Subject<string> = new Subject<string>();
+
 
   constructor(private activatedRoute: ActivatedRoute) { }
+
 
   ngOnInit(): void {
 
     this.photos = this.activatedRoute.snapshot.data['photos'];
 
+    this.debounce.pipe(debounceTime(300)).subscribe(filter => this.filter = filter);
+
+  }
+
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe();
   }
 
 }
